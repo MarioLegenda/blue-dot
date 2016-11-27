d90570c9df55a6aed90f41e6ea4da362235b9783
Mario Škrlec <whitepostmail@gmail.com>
2016-11-27 14:48:35 +0100

    Updated tests for company database

--- tests/Test/CompanyDatabaseTest.php
+++ tests/Test/CompanyDatabaseTest.php
@@ -9,5 +9,7 @@ class CompanyDatabaseTest extends \PHPUnit_Framework_TestCase
     public function testCompanyDatabase()
     {
         $blueDot = new BlueDot(__DIR__.'/config/company_db_config.yml');
+
+        $blueDot->execute('scenario.create_database');
     }
 }
