59eefd23abd65d36b8f2523da6dd826503134faa
Mario Škrlec <whitepostmail@gmail.com>
2016-12-06 20:28:38 +0100

    Deleted unneccessary files

--- src/BlueDot/Configuration/ConfigurationInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace BlueDot\Configuration;
-
-interface ConfigurationInterface
-{
-}
--- src/BlueDot/Configuration/Scenario/ScenarioConfiguration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BlueDot\Configuration\Scenario;
-
-use BlueDot\Common\AbstractArgumentBag;
-
-class ScenarioConfiguration extends AbstractArgumentBag
-{
-}
